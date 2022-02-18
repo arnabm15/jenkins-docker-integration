31809a5a1e8f9175248cec2ad57abf43f01e2a24
arnabm15 <modak.arnab15@gmail.com>
2022-02-18 18:07:55 +0000

    seventh commit

--- src/main/java/com/arnab/jenkins_docker_integration_sample/testIntegration.java
+++ src/main/java/com/arnab/jenkins_docker_integration_sample/testIntegration.java
@@ -3,7 +3,7 @@ package com.arnab.jenkins_docker_integration_sample;
 public class testIntegration {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Hello World..");
 		System.out.println("Testing auto build in jenkins along wih docker.");
 		System.out.println("Testing auto build in jenkins pipeline along wih docker.");
 	}
