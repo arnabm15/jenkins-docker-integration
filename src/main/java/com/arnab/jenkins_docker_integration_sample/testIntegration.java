8ad6949fe7c4434d9f8f5df0b0718627cf0f911e
arnabm15 <modak.arnab15@gmail.com>
2022-02-18 18:31:37 +0000

    Commit to check auto build

--- src/main/java/com/arnab/jenkins_docker_integration_sample/testIntegration.java
+++ src/main/java/com/arnab/jenkins_docker_integration_sample/testIntegration.java
@@ -4,8 +4,7 @@ public class testIntegration {
 
 	public static void main(String[] args) {
 		System.out.println("Hello World..");
-		System.out.println("Testing auto build in jenkins along wih docker.");
-		System.out.println("Testing auto build in jenkins pipeline along wih docker.");
+		System.out.println("Code change applied");
 	}
 
 }
